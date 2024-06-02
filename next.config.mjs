36b3b9f2374fa8fe1e9e6e6276d71798cc399187
isaac <isaac7263@naver.com>
2024-06-02 20:21:49 +0900

    test & change settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: "/isaackimdev.github.io",
+    
     images: {
         unoptimized: true,
     },
